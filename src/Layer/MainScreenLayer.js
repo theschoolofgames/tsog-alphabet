13cd27aee0766069c4996d79a32f01c570644572
Nick <thuydx55vnu@gmail.com>
2015-10-07 13:56:01 +0700

    add assets manager

--- src/Layer/MainScreenLayer.js
+++ src/Layer/MainScreenLayer.js
@@ -30,7 +30,64 @@ var MainScreenLayer = cc.LayerColor.extend({
                     return true;
                 }
         }, this);
+
+        this.downloadAssets();
     },
+
+    downloadAssets: function() {
+        var manifestPath = "project.manifest";
+        var storagePath = ((jsb.fileUtils ? jsb.fileUtils.getWritablePath() : "/") + "assetsManager/");
+        var manager = new jsb.AssetsManager(manifestPath, storagePath);
+
+        cc.log("Storage path for this test : " + storagePath);
+
+        manager.retain();
+
+        if (!manager.getLocalManifest().isLoaded()) {
+            cc.log("Fail to update assets, step skipped.");
+        }
+        else {
+            var listener = new jsb.EventListenerAssetsManager(manager, function(event) {
+                switch (event.getEventCode())
+                {
+                    case jsb.EventAssetsManager.ERROR_NO_LOCAL_MANIFEST:
+                        cc.log("No local manifest file found, skip assets update.");
+                        break;
+                    case jsb.EventAssetsManager.UPDATE_PROGRESSION:
+                        var percent = event.getPercent();
+                        var filePercent = event.getPercentByFile();
+                        cc.log("Download percent : " + percent + " | File percent : " + filePercent);
+                        break;
+                    case jsb.EventAssetsManager.ERROR_DOWNLOAD_MANIFEST:
+                    case jsb.EventAssetsManager.ERROR_PARSE_MANIFEST:
+                        cc.log("Fail to download manifest file, update skipped.");
+                        break;
+                    case jsb.EventAssetsManager.ALREADY_UP_TO_DATE:
+                    case jsb.EventAssetsManager.UPDATE_FINISHED:
+                        cc.log("Update finished.");
+                        // You need to release the assets manager while you are sure you don't need it any more
+                        manager.release();
+                        break;
+                    case jsb.EventAssetsManager.UPDATE_FAILED:
+                        cc.log("Update failed. " + event.getMessage());
+                        // Directly update previously failed assets, we suggest you to count and abort after several retry.
+                        manager.downloadFailedAssets();
+                        break;
+                    case jsb.EventAssetsManager.ERROR_UPDATING:
+                        cc.log("Asset update error: " + event.getAssetId() + ", " + event.getMessage());
+                        break;
+                    case jsb.EventAssetsManager.ERROR_DECOMPRESS:
+                        cc.log(event.getMessage());
+                        break;
+                    default:
+                        break;
+                }
+            });
+
+            cc.eventManager.addListener(listener, 1);
+            manager.update();
+        }
+    }
 });
 
 var MainScreenScene = cc.Scene.extend({
